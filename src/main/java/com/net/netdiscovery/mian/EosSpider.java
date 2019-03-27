267ed02008e181d8ab2b68aea2191414c93e363b
guilin <308139995@qq.com>
2019-03-27 13:30:13 +0800

    代码整理

--- src/main/java/com/net/netdiscovery/mian/EosSpider.java
+++ src/main/java/com/net/netdiscovery/mian/EosSpider.java
@@ -11,20 +11,6 @@ public class EosSpider {
 
     public static void main(String[] args) {
 
-//        Spider.create()
-//                .name("tony")
-//                .url("http://www.useragentstring.com/pages/useragentstring.php?name=Googlebot")
-//                .url("http://www.useragentstring.com/pages/useragentstring.php?name=Baiduspider")
-//                .url("http://www.useragentstring.com/pages/useragentstring.php?name=Chrome")
-//                .url("http://www.useragentstring.com/pages/useragentstring.php?name=Firefox")
-//                .url("http://www.useragentstring.com/pages/useragentstring.php?name=Mozilla")
-//                .url("http://www.useragentstring.com/pages/useragentstring.php?name=Opera")
-//                .url("http://www.useragentstring.com/pages/useragentstring.php?name=Safari")
-//                .url("http://www.useragentstring.com/pages/useragentstring.php?name=Edge")
-//                .parser(new UAParser())
-//                .pipeline(new FilePipeline())
-//                .run();
-
         String eosUrl = "https://www.feixiaohao.com/currencies/eos/";
 
         long periodTime = 1000 * 3;
